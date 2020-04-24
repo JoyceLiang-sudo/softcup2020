6c2fa7063c67e183ace2cfd6709990b7c0cd285c
tabu666 <56100984+tabu666@users.noreply.github.com>
2020-04-24 13:42:34 +0800

    Update detect_color.py

--- model/detect_color.py
+++ model/detect_color.py
@@ -3,54 +3,54 @@ import numpy as np
 
 
 def detect_color(image):
-    """
-    红跟绿有些时候容易混乱，绿灯时有时会识别为red，基本的差不多
-    """
+   
     # image = cv2.imread(image_path)
+    #准确率比上次提高了
+    im_R=image[:,:,0]
+    im_G=image[:,:,1]
+    im_B=image[:,:,2]
+    
+    #平均值
+    im_R_mean=np.mean(im_R)
+    im_G_mean=np.mean(im_G)
+    im_B_mean=np.mean(im_B)
+    color = max(im_R_mean, im_G_mean, im_B_mean)
+    
     # BGR 转成 HSV
     hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    # min and max HSV values
+    # HSV色彩空间阈值
     red_min = np.array([0, 43, 46])
-    red_max = np.array([10, 255, 255])
+    red_max = np.array([16, 255, 255])
     red_min2 = np.array([156, 43, 46])
     red_max2 = np.array([180, 255, 255])
 
-    yello_min = np.array([26, 43, 46])
-    yello_max = np.array([34, 255, 255])
+    yello_min = np.array([17, 43, 46])
+    yello_max = np.array([20, 255, 255])
 
-    green_min = np.array([35, 43, 46])
+    green_min = np.array([21, 43, 46])
     green_max = np.array([77, 255, 255])
 
-    # apply red,yello,green thresh to image
     # 利用cv2.inRange函数设阈值，去除背景部分
     red_thresh = cv2.inRange(hsv_img, red_min, red_max) + cv2.inRange(hsv_img, red_min2, red_max2)
     yellow_thresh = cv2.inRange(hsv_img, yello_min, yello_max)
     green_thresh = cv2.inRange(hsv_img, green_min, green_max)
 
-    # apply blur to fix noise in thresh
-    # 进行中值滤波
-    red_blur = cv2.medianBlur(red_thresh, 5)
-    yello_blur = cv2.medianBlur(yellow_thresh, 5)
-    green_blur = cv2.medianBlur(green_thresh, 5)
+    ##统计非零像素点数
+    red = cv2.countNonZero(red_thresh)
+    yellow = cv2.countNonZero(yellow_thresh)
+    green = cv2.countNonZero(green_thresh)
 
-    # checks which colour thresh has the most white pixels
-    red = cv2.countNonZero(red_blur)
-    yellow = cv2.countNonZero(yello_blur)
-    green = cv2.countNonZero(green_blur)
+    #最大值
+    lightColor = max(red,yellow,green)
+    print(lightColor)
 
-    # the state of the light is the one with the greatest number of white pixels
-    lightColor = max(red, yellow, green)
-    # print(lightColor)
-
-    if lightColor == red:
+    if lightColor == red or color == im_R_mean:
         return 1
-    elif lightColor == yellow:
+    elif lightColor == green or color == im_G_mean:
         return 2
-    elif lightColor == green:
-        return 3
     else:
-        return 0
+        return 3
 
 
 def traffic_light(boxes, img):
@@ -64,9 +64,9 @@ def traffic_light(boxes, img):
             if light_num == 1:
                 box[6] = 'red'
             elif light_num == 2:
-                box[6] = 'yellow'
-            elif light_num == 3:
                 box[6] = 'green'
+            elif light_num == 3:
+                box[6] = 'yello'
             else:
                 box[6] = None
     return boxes
